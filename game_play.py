e65841e7653df4574ba40421d8de9163edf0f98c
26acook2 <26acook2@go.dsdmail.net>
2023-05-01 14:23:47 -0600

    introductions

--- game_play.py
+++ game_play.py
@@ -21,4 +21,42 @@ You also want to take care of yourself, and
 allow time for riding! This game ends at the
 end of the day. Good luck!
 """
-
+Arena_description = """
+Welcome to the arena! This is where you will ride,
+tack up, brush, and more! The arena includes a tying 
+post, mats, and a hose/sprinkler. 
+"""
+Barn_description = """
+Welcome to the barn! This is where all necessary 
+equipment is stored such as: tack, food, treats, etc.
+It includes:
+-halters
+-lead ropes
+-lunge lines
+-bridles
+-saddles
+-saddle pads
+-sinches
+-breast collars
+-brushes
+-hoof picks
+-horse shampoo/condioner
+-hay
+-grain
+-treats
+"""
+Round_Pen_description = """
+Welcome to the round pens! This is where you can round
+pen your horses, either to warm up, or to excersise them.
+You may also use them if your horse needs to be isolated.
+There are no items in them. 
+"""
+Extra_storage_description = """
+Welcome to the extra storage space! This is where all
+obstacles are stored such as: barrels, cones, and poles.
+"""
+Paddock_description = """
+Welcome to teh paddock! This is where your horse roams
+when they aren't being used. It includes water troughs,
+hay nets, and a hose. 
+"""
